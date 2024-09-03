eafb890fc7ee0291013182dbeb69820a4a6a314a
Makol J.Chuol <155411329+Makoljonchuol@users.noreply.github.com>
2024-09-03 18:54:47 +0000

    Ticket class creatd and also methods added

--- _02_02/Ticket.java
+++ _02_02/Ticket.java
@@ -1,12 +1,11 @@
-package _02_03e;
+package _02_02;
 
 public class Ticket {
-
-  public Ticket() {
-
-  }
+  //empty constructor
+  public Ticket(){
+  } 
+  // private field variables
   private String destination;
   private double price;
   private boolean isReturn;
-
 }
